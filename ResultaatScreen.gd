371658b453e995926700b78b51863df12d1843b7
i356100@fhict.nl <n.vandriel@student.fontys.nl>
2020-12-15 16:41:15 +0100

    fixed resultscene
    gefixed dat je nu juiste resultaat krijgt bij onjuist en juiste hoeveelheid

--- ResultaatScreen.gd
+++ ResultaatScreen.gd
@@ -1,8 +1,6 @@
 extends Node2D
 
 
-
-
 func _ready():
 	#weergeven van verhaal 0 = goed 1 = fout
 	$Control/Verhaal1A.text = str(GlobalScripts.VerhaalA1)
@@ -18,22 +16,11 @@ func _ready():
 	
 	#weergeven van Helaas als er meer foute antwoorden zijn dan goede
 	#weergeven van gefeliciteerd als het aantal juiste antwoorden hoger is dan onjuist
-	if $Control/Juist < $Control/Onjuist:
+	if GlobalScripts.Juist < GlobalScripts.Onjuist:
 		$Control/ResultaatTekst.text = ("Helaas")
 		
-	if $Control/Juist > $Control/Onjuist:
+	if GlobalScripts.Onjuist < GlobalScripts.Juist:
 		$Control/ResultaatTekst.text = ("Gefeliciteerd")
 	
-	#if $Control/Juist > $Control/Onjuist:
-	#	$Control/Helaas.hide()
-	#	$Control/Gefeliciteerd.show()
-	#elif $Control/Onjuist > $Control/Juist:
-	#	$Control/Gefeliciteerd.hide()
-	#	$Control/Helaas.show()
-	
-	#if $Control/Onjuist > $Control/Juist:
-		#$Control/Helaas.show()
-	
-	#if $Control/Juist > $Control/Onjuist:
-		#$Control/Gefeliciteerd.show()
+
 
