c468b7a70b778089b0b2a8c5fc3232c2e1630237
i356100@fhict.nl <n.vandriel@student.fontys.nl>
2020-12-16 10:29:27 +0100

    edit: new scene for results
    nieuwe scene gemaakt waarin je fout beantwoorde vragen kun inzien en informatie word getoont over de vraag , ook een back to main menu gemaakt waarin alle variable worden gereset

--- ResultaatScreen.gd
+++ ResultaatScreen.gd
@@ -3,9 +3,9 @@ extends Node2D
 
 func _ready():
 	#weergeven van verhaal 0 = goed 1 = fout
-	$Control/Verhaal1A.text = str(GlobalScripts.VerhaalA1)
-	$Control/Verhaal1B.text = str(GlobalScripts.VerhaalB1)
-	$Control/Verhaal1C.text = str(GlobalScripts.VerhaalC1)
+	#$Control/Verhaal1A.text = str(GlobalScripts.VerhaalA1)
+	#$Control/Verhaal1B.text = str(GlobalScripts.VerhaalB1)
+	#$Control/Verhaal1C.text = str(GlobalScripts.VerhaalC1)
 	
 	#weergeef het aantal juiste en onjuiste antwoorden
 	#groen = juist
@@ -22,5 +22,40 @@ func _ready():
 	if GlobalScripts.Onjuist < GlobalScripts.Juist:
 		$Control/ResultaatTekst.text = ("Gefeliciteerd")
 	
+	#laat de buttons niet zien als de vraag goed beantwoord was
+	if GlobalScripts.VerhaalA1 == 0:
+		$Controlverhaal1/verhaal1.hide()
+		
+	if GlobalScripts.VerhaalB1 == 0:
+		$Controlverhaal2/verhaal2.hide()
+		
+	if GlobalScripts.VerhaalC1 == 0:
+		$Controlverhaal3/verhaal3.hide()
+		
+func _on_verhaal1_pressed():
+	get_tree().change_scene("res://SextingVraag1Info.tscn")
+	
+func _on_verhaal2_pressed():
+	get_tree().change_scene("res://SextingVraag2Info.tscn")
+	
+func _on_verhaal3_pressed():
+	get_tree().change_scene("res://SextingVraag3Info.tscn")
+
+
+#terug naar beginscherm
+func _on_Terug_naar_startscherm_pressed():
+	get_tree().change_scene("res://MainGame.tscn")
+	GlobalScripts.Juist = 0
+	GlobalScripts.Onjuist = 0
+	GlobalScripts.VerhaalA1 = 0
+	GlobalScripts.VerhaalB1 = 0
+	GlobalScripts.VerhaalC1 = 0
+
+
+
+
+
+
+
 
 
